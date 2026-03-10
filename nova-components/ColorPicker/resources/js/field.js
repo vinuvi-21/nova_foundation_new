647cae2a995d811a8a8418b2283ee1103d51e6f3
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-03-10 09:46:24 +0530

    delete some files

--- nova-components/ColorPicker/resources/js/field.js
+++
@@ -1,9 +0,0 @@
-import IndexField from './components/IndexField'
-import DetailField from './components/DetailField'
-import FormField from './components/FormField'
-
-Nova.booting((app, store) => {
-  app.component('index-color-picker', IndexField)
-  app.component('detail-color-picker', DetailField)
-  app.component('form-color-picker', FormField)
-})
