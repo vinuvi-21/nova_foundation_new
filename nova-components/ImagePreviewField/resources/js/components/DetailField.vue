614dd5eac71f1e1c58690bb0808612822500f6b5
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-03-10 09:48:16 +0530

    create image adding field in product listing page

---
+++ nova-components/ImagePreviewField/resources/js/components/DetailField.vue
@@ -0,0 +1,28 @@
+<template>
+  <PanelItem :index="index" :field="field">
+    <template #value>
+      <div class="flex items-center">
+        <span
+          v-for="star in maxStars"
+          :key="star"
+          style="font-size: 30px;"
+          :style="{ color: star <= field.value ? '#f5a623' : '#d3d3d3' }"
+        >
+          ★
+        </span>
+      </div>
+    </template>
+  </PanelItem>
+</template>
+
+<script>
+export default {
+  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+
+  computed: {
+    maxStars() {
+      return this.field.maxStars || 5
+    },
+  },
+}
+</script>
