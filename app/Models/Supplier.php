1c8c5e09339941ba1d56b240e78295ea14fd0d89
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-03-11 15:30:11 +0530

    update supplier model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'country',
+        'category',
+        'status',
+    ];
+
+    
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
