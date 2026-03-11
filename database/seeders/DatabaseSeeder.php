03219fe48c385af9bb958af4cb865979888610c9
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-03-11 16:39:13 +0530

    create a user and add product and supplier seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('password'),
+    ]);
+
         $this->call(RolesTableSeeder::class);
         $this->call(userTableSeeder::class);
         $this->call(RoleUserTableSeeder::class);
         $this->call(RolePermissionTableSeeder::class);
+        $this->call(ProducTSeeder::class);
+        $this->call(SupplierSeeder::class);
+        
     }
 }
